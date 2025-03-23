8250236dfb684b55629ff88494ac77d4c09ea103
Henrik Sejer Pedersen <henrik@sejer.dev>
2025-03-23 11:30:55 +0000

    Refactor Ansible package installation and update bootstrap script to include Homebrew setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y && sudo apt install ansible -y
 
+# Setup homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+ansible-galaxy collection install community.general
+
 ansible-pull -U https://github.com/Tannex/setup-weasel.git --ask-become-pass
